24c33ebf63ac1967ee2e97b472b1d731b553a458
Iman Asgari <imanunkiller10102020@gmail.com>
2022-01-29 11:26:57 +0330

    adding logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,8 +10,21 @@ import {
 } from "@material-ui/core";
 import { ShoppingCart } from "@material-ui/icons";
 
+import logo from "../../assets/game-control.png";
+
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <>
+      <Appbar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title} color="inherit">
+            <img src={logo} />
+            Store Name
+          </Typography>
+        </Toolbar>
+      </Appbar>
+    </>
+  );
 };
 
 export default Navbar;
