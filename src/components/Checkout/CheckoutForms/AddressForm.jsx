950225cfb675faa7a0215c54338f002909ca169b
Iman Asgari <imanunkiller10102020@gmail.com>
2022-02-10 23:03:18 +0330

    Updaing Address form
    Adding styling

--- src/components/Checkout/CheckoutForms/AddressForm.jsx
+++ src/components/Checkout/CheckoutForms/AddressForm.jsx
@@ -7,15 +7,25 @@ import {
   Button,
   Typography,
 } from "@material-ui/core";
+
 import { useForm, FormProvider } from "react-hook-form";
 
+import TextInput from "./TextField";
+
 const AddressForm = () => {
+  const methods = useForm();
   return (
     <>
       <Typography variant="h6" gutterBottom>
         Shipping Address
       </Typography>
-      <FormProvider></FormProvider>
+      <FormProvider {...methods}>
+        <form onSubmit="">
+          <Grid container spacing={3}>
+            <TextInput name="firstName" label="First Name" required />
+          </Grid>
+        </form>
+      </FormProvider>
     </>
   );
 };
