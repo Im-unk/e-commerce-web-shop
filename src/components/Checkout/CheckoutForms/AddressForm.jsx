a14a0b1e379edca46b9c91fbf5cbffa185ee4641
Iman Asgari <imanunkiller10102020@gmail.com>
2022-02-11 22:19:52 +0330

    adding text field to address form

--- src/components/Checkout/CheckoutForms/AddressForm.jsx
+++ src/components/Checkout/CheckoutForms/AddressForm.jsx
@@ -16,13 +16,19 @@ const AddressForm = () => {
   const methods = useForm();
   return (
     <>
-      <Typography variant="h6" gutterBottom>
+      <Typography variant="h6" align="center" gutterBottom>
         Shipping Address
       </Typography>
       <FormProvider {...methods}>
         <form onSubmit="">
           <Grid container spacing={3}>
             <TextInput name="firstName" label="First Name" required />
+            <TextInput name="lastName" label="Last Name" required />
+            <TextInput name="address" label="Address" required />
+            <TextInput name="country" label="Country" required />
+            <TextInput name="city" label="City" required />
+            <TextInput name="postCode" label="Post Code" required />
+            <TextInput name="email" label="Email" required />
           </Grid>
         </form>
       </FormProvider>
--- src/components/Checkout/CheckoutForms/TextField.jsx
+++ src/components/Checkout/CheckoutForms/TextField.jsx
@@ -4,17 +4,18 @@ import { useFormContext, Controller } from "react-hook-form";
 
 const TextInput = ({ name, label, required }) => {
   const { control } = useFormContext();
+  const isError = false;
+
   return (
-    <Grid item xs={12} sm={6}>
-      <Controller
-        as={TextField}
-        control={control}
-        fullWidth
-        name={name}
-        label={label}
-        required={required}
-      />
-    </Grid>
+    <>
+      <Grid item xs={12} sm={6}>
+        <Controller
+          control={control}
+          name={name}
+          render={({ field }) => <TextField fullWidth label={label} required />}
+        />
+      </Grid>
+    </>
   );
 };
 
